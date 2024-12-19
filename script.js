9e3c3e4727d7ecc72175b078d1effbf87c7c3b3d
yanniknt <119662270+yanniknt@users.noreply.github.com>
2024-12-19 23:59:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-let selectedColors = [];
+let selectedColors = JSON.parse(localStorage.getItem('selectedColors')) || [];
 
 function selectColor(questionNumber, color) {
     selectedColors[questionNumber - 1] = color;
-}
-
-function showResult() {
-    const resultDiv = document.getElementById("color-palette");
-    resultDiv.innerHTML = ""; // Reset previous results
-
-    if (selectedColors.length === 4 && selectedColors.every(color => color !== undefined)) {
-        selectedColors.forEach(color => {
-            const colorBox = document.createElement("div");
-            colorBox.style.backgroundColor = color;
-            resultDiv.appendChild(colorBox);
-        });
-
-        alert("Test abgeschlossen! Ihre Farbpalette wurde generiert.");
-    } else {
-        alert("Bitte wählen Sie für jede Musik ein Farbergebnis aus.");
-    }
+    localStorage.setItem('selectedColors', JSON.stringify(selectedColors));
 }
