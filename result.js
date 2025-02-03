d997453b5e6aafeda440f37ff435f889674ad449
yanniknt <119662270+yanniknt@users.noreply.github.com>
2025-02-03 17:20:06 +0100

    Update result.js

--- result.js
+++ result.js
@@ -1,16 +1,22 @@
-window.onload = function() {
+window.onload = function () {
     const selectedColors = JSON.parse(localStorage.getItem('selectedColors')) || [];
 
-    if (selectedColors.length === 4) {
+    if (selectedColors.filter(Boolean).length === 4) {
         const resultDiv = document.getElementById("color-palette");
         resultDiv.innerHTML = "";
 
         selectedColors.forEach(color => {
-            const colorBox = document.createElement("div");
-            colorBox.style.backgroundColor = color;
-            resultDiv.appendChild(colorBox);
+            if (color) {
+                const colorBox = document.createElement("div");
+                colorBox.style.backgroundColor = color;
+                colorBox.style.width = "50px"; 
+                colorBox.style.height = "50px";
+                colorBox.style.display = "inline-block";
+                colorBox.style.margin = "5px";
+                resultDiv.appendChild(colorBox);
+            }
         });
     } else {
-        alert("Es scheint, als hätten Sie nicht alle Farben ausgewählt. Bitte gehen Sie zurück und schließen Sie den Test ab.");
+        alert("Bitte gehen Sie zurück und schließen Sie den Test ab.");
     }
-}
+};
